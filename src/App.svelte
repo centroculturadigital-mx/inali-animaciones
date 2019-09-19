37b60765183ab997d69093ba89abb7a667b31c22
furenku <furenku@gmail.com>
2019-09-19 01:30:36 -0500

    añadir rutas básicas

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+
+	import { Router, Link, Route } from "svelte-routing";
+	import Mar1 from "./animaciones/Mar1.svelte";
+	import Inicio from "./Inicio.svelte";
+
+	export let url = "";
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+
+<Router url="{url}">
+
+	<header class="cabecera-principal">
+		
+		<div class="logotipo">
+			<h4>
+				CCD - ADC
+			</h4>
+		</div>
+
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">Inicio</Link>
+				</li>
+				<li>
+					<Link to="/mar1">Mar 1</Link>
+				</li>
+			</ul>
+		</nav>
+
+	</header>
+
+	<main>
+
+		<Route path="mar1" component="{Mar1}" />
+		<Route path="/" component="{Inicio}" />
+
+	</main>
+
+	<footer>
+		Pie
+	</footer>
+
+</Router>
---
+++ src/Inicio.svelte
@@ -0,0 +1,3 @@
+<h1>
+    Animaciones
+</h1>
