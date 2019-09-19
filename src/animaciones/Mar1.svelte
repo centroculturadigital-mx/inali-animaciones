0dd3ee94490019ea3396149646d7ee7f23d09b8f
furenku <furenku@gmail.com>
2019-09-19 03:31:07 -0500

    implementando olas

--- src/animaciones/Mar1.svelte
+++ src/animaciones/Mar1.svelte
@@ -7,28 +7,70 @@
     let canvas    
 
     //Paper.install(window);
+    let rowH = ( window.innerHeight * 0.9 ) / 10
+    
     onMount(()=>{
       
       
+        let frame;
+        let count=0;
+
+        //let rowH = 
         
+        (function loop() {
+            
+            frame = requestAnimationFrame(loop);
+
+            count++
+
+            if(count%12==0) {
+
+                let ola
+                let olas = []
+
+                let fondo = new Paper.Path.Rectangle(
+                    new Paper.Point(0,0),
+                    new Paper.Point(window.innerWidth,window.innerHeight),
+                );
+
+                fondo.fillColor = new Paper.Color(0.01,0.3,0.7+(count%130/1300));
+
+                //Define array of paths (I've choose 12 because my multitouch table accept 12 touch max)
+                for (var i = 0; i < 10; i++)
+                {
+                    ola = new Paper.Path();
+                    
+                    ola.strokeColor = new Paper.Color((i*count%90)/900,(i%8*13)/500,0.3+i/15);
+                    ola.strokeWidth = rowH / 1.6;
+                    for (var j = 0; j < 30; j++)
+                    {
+                        let point = new Paper.Point(
+                            j*30,
+                            (i*rowH) - Math.cos(j-count)*20 * Math.sin(
+                                (j*2) + count%36)
+                            )                
+                        //let point = new Paper.Point(j*33,i*30+Math.sin(count%360+j*30))                
+                        ola.add( point )
+                        // ola.smooth()
+                    }
+
+                    olas.push(ola);
+                }
+
+            }
+                
+            
+        }());
+
+/*         return () => {
+            cancelAnimationFrame(frame);
+        };
+ */       
+
+            
         Paper.setup(canvas);
 
-        let myPath
-        let myPaths = []
-        //Define array of paths (I've choose 12 because my multitouch table accept 12 touch max)
-        for (var i = 0; i < 12; i++)
-        {
-            myPath = new Paper.Path();
-            myPath.strokeColor = '#e0e0e0';
-            myPath.strokeWidth = 30;
-            for (var j = 0; j < 12; j++)
-            {
-                let point = new Paper.Point(j*30,i*40+j*20)                
-                myPath.add( point )
-            }
-
-            myPaths.push(myPath);
-        }
+        
         
     })
     
@@ -37,16 +79,12 @@
 
 <style>
 	canvas {
-		width: 100%;
-		height: 100%;
+		width: 100vw;
+		height: 90vh;
 		background-color: #eee;
 	}
 </style>
 
-
-<h1>
-    Mar 1
-</h1>
 
 <canvas
 id="canvas"
